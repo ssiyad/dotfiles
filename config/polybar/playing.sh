fa24d5c3b0c52fac7523eaac826712fad49c0d1f
Sabu Siyad <ssiyad@tuta.io>
2019-11-30 19:06:10 +0530

    Init

---
+++ config/polybar/playing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$(playerctl status)" = "Playing" ]; then
+  title=`exec playerctl metadata xesam:title`
+  artist=`exec playerctl metadata xesam:artist`
+  echo "$title - $artist"
+else
+  echo ""
+fi
+
+
