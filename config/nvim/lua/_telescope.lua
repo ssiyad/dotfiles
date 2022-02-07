6ed943ee2e1817139223d28c54d66bab1579ce44
Sabu Siyad <hello@ssiyad.com>
2022-02-08 02:06:01 +0530

    neovim: bind: telescope

--- config/nvim/lua/_telescope.lua
+++ config/nvim/lua/_telescope.lua
@@ -8,3 +8,10 @@ require('telescope').setup {
 }
 
 require('telescope').load_extension('fzy_native')
+
+vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>fr', ':Telescope lsp_references<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>ft', ':TodoTelescope<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>gc', ':Telescope git_commits<CR>', { noremap = true })
+
