63dcde9b629ab79bf5bb5dfe9fe83b5397845277
Sabu Siyad <hello@ssiyad.com>
2022-02-08 02:11:48 +0530

    neovim: bind: gitsigns

--- config/nvim/lua/_gitsigns.lua
+++ config/nvim/lua/_gitsigns.lua
@@ -7,3 +7,9 @@ require('gitsigns').setup({
         changedelete = {hl = 'GitSignsChange', text = '█', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     },
 })
+
+vim.api.nvim_set_keymap('n', '<Leader>hs', ':Gitsigns stage_hunk<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>hu', ':Gitsigns undo_stage_hunk<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>hp', ':Gitsigns preview_hunk<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>hr', ':Gitsigns reset_hunk<CR>', { noremap = true })
+
