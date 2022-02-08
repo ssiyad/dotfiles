bc8998db16ad48d5b998693259b59f975e2e0c59
Sabu Siyad <hello@ssiyad.com>
2022-02-08 21:50:29 +0530

    neovim: bind: lspconfig: prefix leader

--- config/nvim/lua/_lspconfig.lua
+++ config/nvim/lua/_lspconfig.lua
@@ -25,13 +25,13 @@ for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup(
         coq.lsp_ensure_capabilities({
             on_attach = function(client, bufnr)
-                local opts = { noremap=true, silent=true }
+                local opts = { noremap = true, silent = true }
 
                 vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 
                 require 'lsp_signature'.on_attach()
             end,
