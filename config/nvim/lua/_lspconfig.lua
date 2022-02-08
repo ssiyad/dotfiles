ab3c810e5a3c6dffde91b0be5d91f4e7e79eea33
Sabu Siyad <hello@ssiyad.com>
2022-02-08 19:31:02 +0530

    neovim: bind: lspconfig

--- config/nvim/lua/_lspconfig.lua
+++ config/nvim/lua/_lspconfig.lua
@@ -25,6 +25,14 @@ for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup(
         coq.lsp_ensure_capabilities({
             on_attach = function(client, bufnr)
+                local opts = { noremap=true, silent=true }
+
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                vim.api.nvim_buf_set_keymap(bufnr, 'n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+
                 require 'lsp_signature'.on_attach()
             end,
             flags = {
