8a02cb28a1bf60fdfad192c1dde4a0f05677fa31
Sabu Siyad <hello@ssiyad.com>
2022-05-30 02:33:16 +0530

    nvim: lsp: modifications (mostly ui)
    https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization

--- config/nvim/lua/_lspconfig.lua
+++ config/nvim/lua/_lspconfig.lua
@@ -33,6 +33,84 @@ for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup(
         coq.lsp_ensure_capabilities({
             on_attach = function(client, bufnr)
+                vim.diagnostic.config({
+                    virtual_text = false,
+                    signs = true,
+                    underline = true,
+                    update_in_insert = false,
+                    severity_sort = false,
+                })
+
+                vim.api.nvim_create_autocmd("CursorHold", {
+                    buffer = bufnr,
+                    callback = function()
+                        local opts = {
+                            focusable = false,
+                            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                            border = 'rounded',
+                            source = 'always',
+                            prefix = ' ',
+                            scope = 'cursor',
+                        }
+                        vim.diagnostic.open_float(nil, opts)
+                    end
+                })
+
+                local function goto_definition(split_cmd)
+                    local util = vim.lsp.util
+                    local log = require("vim.lsp.log")
+                    local api = vim.api
+
+                    local handler = function(_, result, ctx)
+                        if result == nil or vim.tbl_isempty(result) then
+                            local _ = log.info() and log.info(ctx.method, "No location found")
+                            return nil
+                        end
+
+                        if split_cmd then
+                            vim.cmd(split_cmd)
+                        end
+
+                        if vim.tbl_islist(result) then
+                            util.jump_to_location(result[1])
+
+                            if #result > 1 then
+                                util.set_qflist(util.locations_to_items(result))
+                                api.nvim_command("copen")
+                                api.nvim_command("wincmd p")
+                            end
+                        else
+                            util.jump_to_location(result)
+                        end
+                    end
+
+                    return handler
+                end
+
+                vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
+
+                if client.server_capabilities.documentHighlightProvider then
+                    vim.cmd [[
+                    ]]
+                    vim.api.nvim_create_augroup('lsp_document_highlight', {
+                        clear = false
+                    })
+                    vim.api.nvim_clear_autocmds({
+                        buffer = bufnr,
+                        group = 'lsp_document_highlight',
+                    })
+                    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                        group = 'lsp_document_highlight',
+                        buffer = bufnr,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+                    vim.api.nvim_create_autocmd('CursorMoved', {
+                        group = 'lsp_document_highlight',
+                        buffer = bufnr,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+
                 local opts = { noremap = true, silent = true }
 
                 vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
