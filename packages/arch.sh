367c8a6152e605e2465ef7142bab1bcec3c14bbc
Sabu Siyad <hello@ssiyad.com>
2022-06-04 00:15:28 +0530

    packages: cleanup

--- packages/arch.sh
+++ packages/arch.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-pacman -Qqen > pacman.list
-pacman -Qqem > aur.list
+for group in `cat groups.list`; do
+    echo '' > /tmp/pacman_groups_members
+    pacman -Sqg $group >> /tmp/pacman_groups_members
+done
+
+pacman -Qqen > pacman.list 
+echo "$(comm -23 pacman.list /tmp/pacman_groups_members)" > pacman.list
+sed -i -e 's/\s\+/\n/g' pacman.list
+rm /tmp/pacman_groups_members
+
+pacman -Qqem > aur.list 
 
