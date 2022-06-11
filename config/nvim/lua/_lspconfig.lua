32a9145070f29cf4eed17613f376edc7180ddf29
Sabu Siyad <hello@ssiyad.com>
2022-06-12 00:24:01 +0530

    nvim: lsp: add Leader

--- config/nvim/lua/_lspconfig.lua
+++ config/nvim/lua/_lspconfig.lua
@@ -174,12 +174,12 @@ local function on_attach(client, bufnr)
     local opts = { noremap = true, silent = true, buffer = bufnr }
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<Leader>gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', '<Leader>gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<Leader>gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<Leader>gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
 end
 
 for _, lsp in pairs(servers) do
