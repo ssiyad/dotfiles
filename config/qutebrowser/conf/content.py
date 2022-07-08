96798db2b7299ada24a3a8b4e5b8a055ed9f795d
Sabu Siyad <hello@ssiyad.com>
2022-07-08 16:05:14 +0530

    qutebrowser: enable adblock
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/qutebrowser/conf/content.py
+++ config/qutebrowser/conf/content.py
@@ -3,3 +3,8 @@ c = c.content
 c.pdfjs = True
 c.prefers_reduced_motion = True
 c.autoplay = False
+c.blocking.adblock.lists = [
+    'https://easylist.to/easylist/easylist.txt',
+    'https://easylist.to/easylist/easyprivacy.txt',
+    'https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts',
+]
