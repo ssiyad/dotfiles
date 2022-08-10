f368b23878c4d6a4a770e90667b5dfd80bf07282
Sabu Siyad <hello@ssiyad.com>
2022-08-10 10:19:03 +0530

    nvim: cmp buffer: get from all visible buffers
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -79,7 +79,16 @@ local sources = cmp.config.sources({
     },
     {
         name = 'buffer',
-        group_index = 4
+        group_index = 4,
+        option = {
+            get_bufnrs = function()
+                local bufs = {}
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    bufs[vim.api.nvim_win_get_buf(win)] = true
+                end
+                return vim.tbl_keys(bufs)
+            end
+        }
     }
 })
 
