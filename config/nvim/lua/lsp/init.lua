789f919b1a148052e8735d6c3498169f1c348d7a
Sabu Siyad <hello@ssiyad.com>
2022-08-31 23:10:37 +0530

    nvim: add sumneko_lua
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -3,6 +3,7 @@ require('lsp.clients.gopls')
 require('lsp.clients.prismals')
 require('lsp.clients.pyright')
 require('lsp.clients.rust_analyzer')
+require('lsp.clients.sumneko_lua')
 require('lsp.clients.tailwindcss')
 require('lsp.clients.tsserver')
 
