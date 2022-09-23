f1a00f78c90a5863104d0728f479d3709d67523c
Sabu Siyad <hello@ssiyad.com>
2022-09-23 13:40:25 +0530

    nvim(lsp/rust-analyzer): analyzer from rustup
    - `rustup component add rust-analyzer`
    - `rustup run stable rust-analyzer`
    - https://blog.rust-lang.org/2022/09/22/Rust-1.64.0.html
    
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/lsp/clients/rust_analyzer.lua
+++ config/nvim/lua/lsp/clients/rust_analyzer.lua
@@ -1,5 +1,6 @@
 require('lspconfig').rust_analyzer.setup {
     on_attach = require 'lsp.on_attach',
-    handlers = require 'lsp.handlers'
+    handlers = require 'lsp.handlers',
+    cmd = { 'rustup', 'run', 'stable', 'rust-analyzer' }
 }
 
