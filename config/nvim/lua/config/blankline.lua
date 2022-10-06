d80cf7c25257c5cc7f8076f11149a0fbdd7faa29
Sabu Siyad <hello@ssiyad.com>
2022-10-06 10:38:28 +0530

    nvim: update `indent-blankline` config
    - remove explicit `IndentBlanklineContextChar` color
    - remove indent start underline
    
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/config/blankline.lua
+++ config/nvim/lua/config/blankline.lua
@@ -1,15 +1,6 @@
 require('indent_blankline').setup {
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
     show_end_of_line = false,
 }
-
-local colors = require("tokyonight.colors").setup()
-
-vim.cmd(
-    string.format(
-        [[ highlight IndentBlanklineContextChar guifg=%s gui=nocombine ]],
-        colors.purple
-    )
-) 
 
