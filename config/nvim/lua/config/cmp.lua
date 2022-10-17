510e112010122d243c83fe4032eeb96a730b4a60
Sabu Siyad <hello@ssiyad.com>
2022-10-18 01:12:58 +0530

    nvim: cmp_lsp: use `default_capabilites()`
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/config/cmp.lua
+++ config/nvim/lua/config/cmp.lua
@@ -8,7 +8,7 @@ local luasnip = require('luasnip')
 local has_words_before = require('utils.has_words_before')
 
 -- update capabilities
-require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+require('cmp_nvim_lsp').default_capabilities()
 
 local snippet = {
     expand = function(args)
