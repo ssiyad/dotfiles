8add64e540aa2ac1728eb710839b2f850375a757
Sabu Siyad <hello@ssiyad.com>
2022-11-20 19:46:33 +0530

    nvim(lsp): tailwindcss: use default filetypes
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/lsp/clients/tailwindcss.lua
+++ config/nvim/lua/lsp/clients/tailwindcss.lua
@@ -1,9 +1,5 @@
 return require('lspconfig').tailwindcss.setup {
     capabilities = require 'lsp.capabilites' (),
-    filetypes = { 
-        "javascriptreact",
-        "typescriptreact",
-    },
     on_attach = require 'lsp.on_attach',
     handlers = require 'lsp.handlers'
 }
