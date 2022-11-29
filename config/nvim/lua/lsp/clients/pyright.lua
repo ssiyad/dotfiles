8b7dc2e0a29124b761f70339e42d4cf94d316c7a
Sabu Siyad <hello@ssiyad.com>
2022-11-30 00:14:04 +0530

    nvim: pyright: turn off type checking
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/nvim/lua/lsp/clients/pyright.lua
+++ config/nvim/lua/lsp/clients/pyright.lua
@@ -1,6 +1,14 @@
 require('lspconfig').pyright.setup {
-    capabilities = require 'lsp.capabilites' (),
-    handlers = require 'lsp.handlers',
-    on_attach = require 'lsp.on_attach',
+    capabilities = require('lsp.capabilites')(),
+    handlers = require('lsp.handlers'),
+    on_attach = require('lsp.on_attach'),
+    settings = {
+        python = {
+            analysis = {
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "off",
+            },
+        },
+    },
 }
 
