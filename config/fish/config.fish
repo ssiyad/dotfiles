5526733729192d923693c82eea2c782d5ff7e36e
Sabu Siyad <hello@ssiyad.com>
2022-12-09 15:56:12 +0530

    fish: set editor nvim
    Signed-off-by: Sabu Siyad <hello@ssiyad.com>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,14 +1,12 @@
-if status is-interactive
-    alias mkdir="mkdir -pv"
-    alias ls="ls -aFGh --color=always"
-    alias rm="rm -rfv"
-    alias mv="mv -iv"
-    alias cp="cp -rv"
-    alias ix.io="curl -F 'f:1=<-' ix.io"
+alias mkdir="mkdir -pv"
+alias ls="ls -aFGh --color=always"
+alias rm="rm -rfv"
+alias mv="mv -iv"
+alias cp="cp -rv"
+alias ix.io="curl -F 'f:1=<-' ix.io"
 
-    abbr v "nvim"
-    abbr g "git"
+abbr v "nvim"
+abbr g "git"
 
-    set fish_greeting
-end
-
+set fish_greeting
+set -x EDITOR nvim
