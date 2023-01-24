bcdd73058e62f116f777fbe678e187d45fb6d6ee
Sabu Siyad <hello@ssiyad.com>
2023-01-24 15:05:08 +0530

    nvim(neogen): use `reST` for python

--- config/nvim/lua/config/neogen.lua
+++ config/nvim/lua/config/neogen.lua
@@ -1,6 +1,15 @@
 local neogen = require('neogen')
 local keymap_set = require('utils.keymap_set')
 
-neogen.setup({})
+neogen.setup({
+    enabled = true,
+    languages = {
+        python = {
+            template = {
+                annotation_convention = 'reST',
+            },
+        },
+    },
+})
 
 keymap_set('n', '<Leader>d', neogen.generate)
