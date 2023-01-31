b18f9d7b0269572f8e00379fbb8f2927c218df5e
Sabu Siyad <hello@ssiyad.com>
2023-01-31 21:53:03 +0530

    nvim(lsp): volar: point to ts libs

--- config/nvim/lua/lsp/clients/volar.lua
+++ config/nvim/lua/lsp/clients/volar.lua
@@ -7,6 +7,11 @@ require('lspconfig').volar.setup({
 		'vue',
 		'json',
 	},
+	init_options = {
+		typescript = {
+			tsdk = "/opt/homebrew/lib/node_modules/typescript/lib/"
+		},
+	},
     on_attach = require 'lsp.on_attach',
     handlers = require 'lsp.handlers',
 })
