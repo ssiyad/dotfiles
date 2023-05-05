d91fe1986781a96a53f3e322e623f68e1cc2838b
Sabu Siyad <hello@ssiyad.com>
2023-05-05 22:43:13 +0530

    nvim: update settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -2,14 +2,13 @@ local opt = vim.opt
 
 opt.autoindent = true
 opt.autoread = true
-opt.cmdheight = 1
-opt.completeopt = 'menu,menuone,noselect'
+opt.cmdheight = 0
 opt.cursorline = true
-opt.ff = 'unix'
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
+opt.ff = "unix"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 99
-opt.foldmethod = 'expr'
-opt.guicursor = ''
+opt.foldmethod = "expr"
+opt.guicursor = ""
 opt.ignorecase = true
 opt.incsearch = true
 opt.laststatus = 3
@@ -17,10 +16,13 @@ opt.number = true
 opt.relativenumber = true
 opt.shiftwidth = 0
 opt.showmode = false
-opt.signcolumn = 'yes:1'
+opt.signcolumn = "yes:1"
 opt.smartcase = true
 opt.smartindent = true
 opt.spell = true
 opt.tabstop = 4
 opt.termguicolors = true
 opt.updatetime = 750
+
+-- https://github.com/neovim/neovim/pull/22668
+vim.loader.enable()
