77583fc8698e69bec707cd34304ee9e017298c9b
Sabu Siyad <hello@ssiyad.com>
2023-05-08 13:47:34 +0530

    nvim: neorg: add dirman

--- config/nvim/lua/config/neorg.lua
+++ config/nvim/lua/config/neorg.lua
@@ -6,6 +6,15 @@ require("neorg").setup({
 				icon_preset = "diamond",
 			},
 		},
+		["core.dirman"] = {
+			config = {
+				workspaces = {
+					diary = "~/neorg-diary",
+					work = "~/neorg-work",
+				},
+				index = "index.norg",
+			},
+		},
 		["core.completion"] = {
 			config = {
 				engine = "nvim-cmp",
