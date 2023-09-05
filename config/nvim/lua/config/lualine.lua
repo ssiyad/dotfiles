ecb57c3018a0c0d241ca3812f781ae2c1101551f
Sabu Siyad <hello@ssiyad.com>
2023-09-06 00:39:51 +0530

    nvim: lualine: update config

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -1,17 +1,17 @@
 require("lualine").setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        always_divide_middle = true,
-    },
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-    },
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff" },
+		lualine_c = { "filename" },
+		lualine_x = { "diagnostics" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
 })
