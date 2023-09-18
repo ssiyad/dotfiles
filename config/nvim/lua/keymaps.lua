db5dfcce2622ce086b57ff26e115ad546216baaa
Sabu Siyad <hello@ssiyad.com>
2023-09-18 12:11:45 +0530

    nvim: remove hl search bindings

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,12 +1,4 @@
 local keymap_set = require("utils.keymap_set")
 
-keymap_set("n", "n", [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]])
-keymap_set("n", "N", [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]])
-keymap_set("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]])
-keymap_set("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]])
-keymap_set("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]])
-keymap_set("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]])
-
 keymap_set("n", "<Leader>l", ":noh<CR>")
-
 keymap_set("n", "<Leader>t", ":vs | term<CR> | i")
