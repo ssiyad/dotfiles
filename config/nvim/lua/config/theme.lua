d7403a5a0eaf09f46688e101f718a98e1056db06
Sabu Siyad <hello@ssiyad.com>
2024-01-25 18:11:44 +0530

    nvim: lazy sync

--- config/nvim/lua/config/theme.lua
+++ config/nvim/lua/config/theme.lua
@@ -1,25 +1,26 @@
-require('catppuccin').setup({
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        markdown = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        ts_rainbow = true,
-    },
-    custom_highlights = function(colors)
-        return {
-            FloatBorder = { bg = colors.mantle },
-            NvimTreeNormal = { bg = colors.none },
-            TelescopeBorder = { bg = colors.mantle },
-            TelescopeNormal = { bg = colors.mantle },
-            TelescopePreviewTitle = { bg = colors.blue , fg = colors.base },
-            TelescopePromptTitle = { bg = colors.red, fg = colors.base },
-        }
-    end
+local catppuccin = require("catppuccin")
+
+catppuccin.setup({
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		markdown = true,
+		nvimtree = true,
+		telescope = true,
+		treesitter = true,
+		treesitter_context = true,
+		ts_rainbow = true,
+	},
+	custom_highlights = function(colors)
+		return {
+			FloatBorder = { bg = colors.mantle },
+			NvimTreeNormal = { bg = colors.none },
+			TelescopeBorder = { bg = colors.mantle },
+			TelescopeNormal = { bg = colors.mantle },
+			TelescopePreviewTitle = { bg = colors.blue, fg = colors.base },
+			TelescopePromptTitle = { bg = colors.red, fg = colors.base },
+		}
+	end,
 })
 
-vim.cmd.colorscheme('catppuccin')
-
+vim.cmd.colorscheme("catppuccin")
