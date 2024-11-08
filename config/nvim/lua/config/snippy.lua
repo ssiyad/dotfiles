3bbe5402e45dada40f9f69ae2e22197e924434e1
Sabu Siyad <hello@ssiyad.com>
2024-11-08 08:46:55 +0530

    nvim: snippet for ts react components

--- config/nvim/lua/config/snippy.lua
+++ config/nvim/lua/config/snippy.lua
@@ -1,11 +1,15 @@
-require('snippy').setup({
+require("snippy").setup({
 	scopes = {
 		_ = function(scopes)
-			table.insert(scopes, 'misc')
+			table.insert(scopes, "misc")
 			return scopes
 		end,
 		typescript = function(scopes)
-			table.insert(scopes, 'javascript')
+			table.insert(scopes, "javascript")
+			return scopes
+		end,
+		typescriptreact = function(scopes)
+			table.insert(scopes, "javascript")
 			return scopes
 		end,
 	},
