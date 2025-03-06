aee2c79c48225729f2cea74653caa477ca9b6baf
Sabu Siyad <hello@ssiyad.com>
2025-03-06 11:01:35 +0530

    nvim: chore: update laz bootstrap

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,17 +1,17 @@
 local git_repo = 'git@github.com:folke/lazy.nvim.git'
 local install_path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
-if not vim.loop.fs_stat(install_path) then
+if not (vim.uv or vim.loop).fs_stat(install_path) then
   vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
-    '--single-branch',
+    '--branch=stable',
     git_repo,
     install_path,
   })
 end
 
-vim.opt.runtimepath:prepend(install_path)
+vim.opt.rtp:prepend(install_path)
 
 require('lazy').setup('plugins.list')
