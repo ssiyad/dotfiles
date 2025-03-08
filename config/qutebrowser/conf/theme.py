495ef936137fa8cf147401164bc5451bdf8e6296
Sabu Siyad <hello@ssiyad.com>
2025-03-08 17:03:12 +0530

    chore: qutebrowser: catppuccin!

---
+++ config/qutebrowser/conf/theme.py
@@ -0,0 +1,7 @@
+import catppuccin
+
+# set the flavor you'd like to use
+# valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+# last argument (optional, default is False): enable the plain look for the
+# menu rows
+catppuccin.setup(c, "mocha", True)
