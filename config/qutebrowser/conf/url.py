6f2076e0b76b126da048caee76feff3fce551bcb
Sabu Siyad <hello@ssiyad.com>
2025-09-14 23:44:05 +0530

    qutebrowser: change default page to bookmarks page

--- config/qutebrowser/conf/url.py
+++ config/qutebrowser/conf/url.py
@@ -1,5 +1,5 @@
-c.url.default_page = "https://google.com"
-c.url.start_pages = "https://google.com"
+c.url.default_page = "qute://bookmarks/"
+c.url.start_pages = "qute://bookmarks/"
 c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?q={}",
     "aw": "https://wiki.archlinux.org/?search={}",
