3761b8ace088058390cb7dc0aba9b7d26f5bd985
Sabu Siyad <hello@ssiyad.com>
2025-10-22 00:08:18 +0530

    qutebrowser: add arch packages url

--- config/qutebrowser/conf/url.py
+++ config/qutebrowser/conf/url.py
@@ -3,6 +3,7 @@ c.url.start_pages = "qute://bookmarks/"
 c.url.searchengines = {
     "DEFAULT": "https://www.google.com/search?q={}",
     "aw": "https://wiki.archlinux.org/?search={}",
+    "ap": "https://archlinux.org/packages/?q={}",
     "s": "https://stackoverflow.com/search?q={}",
     "w": "https://en.wikipedia.org/wiki/Special:Search?search={}",
     "y": "https://www.youtube.com/results?search_query={}",
