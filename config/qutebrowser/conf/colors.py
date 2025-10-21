ca286dfd8966dbf57c88d8b53ef63b9dcc2f35ea
Sabu Siyad <hello@ssiyad.com>
2025-10-22 00:23:02 +0530

    qutebrowser: add catppuccin

--- config/qutebrowser/conf/colors.py
+++ config/qutebrowser/conf/colors.py
@@ -1,10 +1,10 @@
-# import catppuccin
+import catppuccin
 
 # set the flavor you'd like to use
 # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
 # last argument (optional, default is False): enable the plain look for the
 # menu rows
-# catppuccin.setup(c, "mocha", True)
+catppuccin.setup(c, "mocha", True)
 
 c.colors.webpage.darkmode.enabled = True
 c.colors.webpage.darkmode.algorithm = 'lightness-hsl'
