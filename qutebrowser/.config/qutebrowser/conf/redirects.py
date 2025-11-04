0661a102ba6313b9690eba7cd07abdf64038bf88
Sabu Siyad <hello@ssiyad.com>
2025-11-04 11:10:53 +0530

    feat(qutebrowser): redirect for hacker news pages (single)

--- qutebrowser/.config/qutebrowser/conf/redirects.py
+++ qutebrowser/.config/qutebrowser/conf/redirects.py
@@ -1,17 +1,24 @@
 from qutebrowser.api import interceptor
-
-targets = {
-    "www.reddit.com": "old.reddit.com",
-}
+from qutebrowser.qt.core import QUrl, QUrlQuery
 
 
-def rewrite(request: interceptor.Request):
-    if request.request_url.host() in targets:
-        request.request_url.setHost(targets[request.request_url.host()])
-        try:
-            request.redirect(request.request_url)
-        except interceptor.interceptors.RedirectException:
-            pass
+def old_reddit(request: interceptor.Request):
+    if request.request_url.host() == "www.reddit.com":
+        request.request_url.setHost("old.reddit.com")
+        request.redirect(request.request_url)
 
 
-interceptor.register(rewrite)
+def hckrnws(request: interceptor.Request):
+    if (
+        request.request_url.host() == "news.ycombinator.com"
+        and request.request_url.path() == "/item"
+    ):
+        query = QUrlQuery(request.request_url)
+        item_id = query.queryItemValue("id")
+        if item_id:
+            new_url = QUrl(f"https://www.hckrnws.com/stories/{item_id}")
+            request.redirect(new_url)
+
+
+interceptor.register(old_reddit)
+interceptor.register(hckrnws)
