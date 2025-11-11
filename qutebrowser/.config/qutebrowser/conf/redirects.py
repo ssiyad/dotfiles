71611fc88bdada5aa4a6e1b5222a5dc0f48e2638
Sabu Siyad <hello@ssiyad.com>
2025-11-11 21:22:23 +0530

    fix(qutebrowser): redirects
    - new to old reddit
    - hackernews

--- qutebrowser/.config/qutebrowser/conf/redirects.py
+++ qutebrowser/.config/qutebrowser/conf/redirects.py
@@ -3,21 +3,40 @@ from qutebrowser.qt.core import QUrl, QUrlQuery
 
 
 def old_reddit(request: interceptor.Request):
-    if request.request_url.host() == "www.reddit.com":
+    if not request.request_url.host() == "www.reddit.com":
+        return
+    try:
         request.request_url.setHost("old.reddit.com")
         request.redirect(request.request_url)
+    except interceptor.interceptors.RedirectException:
+        pass
 
 
 def hckrnws(request: interceptor.Request):
-    if (
-        request.request_url.host() == "news.ycombinator.com"
-        and request.request_url.path() == "/item"
-    ):
-        query = QUrlQuery(request.request_url)
-        item_id = query.queryItemValue("id")
-        if item_id:
-            new_url = QUrl(f"https://www.hckrnws.com/stories/{item_id}")
-            request.redirect(new_url)
+    if not request.request_url.host() == "news.ycombinator.com":
+        return
+
+    path = request.request_url.path().lstrip("/")
+    url: QUrl | None = None
+
+    match path:
+        case "newest":
+            url = QUrl("https://www.hckrnws.com/new/1")
+        case "show":
+            url = QUrl("https://www.hckrnws.com/show/1")
+        case "ask":
+            url = QUrl("https://www.hckrnws.com/ask/1")
+        case "item":
+            query = QUrlQuery(request.request_url)
+            id = query.queryItemValue("id")
+            url = QUrl(f"https://www.hckrnws.com/stories/{id}")
+        case _:
+            url = QUrl("https://www.hckrnws.com/top/1")
+
+    try:
+        request.redirect(url)
+    except interceptor.interceptors.RedirectException:
+        pass
 
 
 interceptor.register(old_reddit)
