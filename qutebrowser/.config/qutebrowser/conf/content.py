8711ca047956377fe0e932e0487753a31e9aa690
Sabu Siyad <hello@ssiyad.com>
2025-11-23 14:03:57 +0530

    fix(qutebrowser): move hosts list to its own property

--- qutebrowser/.config/qutebrowser/conf/content.py
+++ qutebrowser/.config/qutebrowser/conf/content.py
@@ -4,6 +4,9 @@ c.content.autoplay = False
 c.content.blocking.adblock.lists = [
     "https://easylist.to/easylist/easylist.txt",
     "https://easylist.to/easylist/easyprivacy.txt",
+    "https://easylist.to/easylist/fanboy-social.txt",
+]
+c.content.blocking.hosts.lists = [
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts",
 ]
