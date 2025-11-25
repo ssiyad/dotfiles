bd6d5e84e180ee8f59d83b179c5ae31b7531d92d
Sabu Siyad <hello@ssiyad.com>
2025-11-26 00:01:48 +0530

    feat(qutebrowser): play video in mpv

--- qutebrowser/.config/qutebrowser/conf/bindings.py
+++ qutebrowser/.config/qutebrowser/conf/bindings.py
@@ -8,3 +8,6 @@ config.bind('\\\\', 'spawn --userscript qute-pass --mode gopass --password-store
 config.bind('\\u', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --username-only')
 config.bind('\\p', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --password-only')
 config.bind('\\o', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --otp-only')
+
+# Media
+config.bind("pv", "hint links spawn --detach mpv {hint-url}")
