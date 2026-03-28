7b0460d5f24733caeb30b16dcb61ba8ac9c6e6a3
Sabu Siyad <hello@ssiyad.com>
2026-03-29 00:48:31 +0530

    replace rofi with bemenu

--- qutebrowser/.config/qutebrowser/conf/bindings.py
+++ qutebrowser/.config/qutebrowser/conf/bindings.py
@@ -1,13 +1,19 @@
-# b = config.bind
-# b('pa', 'spawn --detach mpv --no-video {url}')
-# b('pla', 'hint links spawn --detach mpv --no-video {hint-url}')
-# b('plv', 'hint links spawn --detach mpv {hint-url}')
-# b('pv', 'spawn --detach mpv {url}')
-
-config.bind('\\\\', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root')
-config.bind('\\u', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --username-only')
-config.bind('\\p', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --password-only')
-config.bind('\\o', 'spawn --userscript qute-pass --mode gopass --password-store ~/.local/share/gopass/stores/root --otp-only')
+config.bind(
+    "\\\\",
+    "spawn --userscript qute-pass --mode gopass --dmenu-invocation 'bemenu --prompt passwords' --password-store ~/.local/share/gopass/stores/root",
+)
+config.bind(
+    "\\u",
+    "spawn --userscript qute-pass --mode gopass --dmenu-invocation 'bemenu --prompt passwords' --password-store ~/.local/share/gopass/stores/root --username-only",
+)
+config.bind(
+    "\\p",
+    "spawn --userscript qute-pass --mode gopass --dmenu-invocation 'bemenu --prompt passwords' --password-store ~/.local/share/gopass/stores/root --password-only",
+)
+config.bind(
+    "\\o",
+    "spawn --userscript qute-pass --mode gopass --dmenu-invocation 'bemenu --prompt passwords' --password-store ~/.local/share/gopass/stores/root --otp-only",
+)
 
 # Media
 config.bind("pv", "hint links spawn --detach mpv {hint-url}")
