84430cead1fd82dae399452b7c7d9ba7150e2ab4
Sabu Siyad <hello@ssiyad.com>
2026-04-11 17:01:12 +0530

    remove tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,15 +9,3 @@ abbr g "git"
 
 set fish_greeting
 set -x EDITOR nvim
-
-function ts
-	set session_name (basename (pwd))
-	if set -q TMUX
-		echo 'Already inside a tmux session'
-		exit 1
-	else if tmux has-session -t $session_name
-		tmux attach-session -t $session_name
-	else
-		tmux new-session -s $session_name
-	end
-end
