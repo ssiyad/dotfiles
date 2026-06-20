f8e142caa229d14e42ebbe36630b6c61b10442ce
Sabu Siyad <hello@ssiyad.com>
2026-06-20 18:22:48 +0530

    qutebrowser: open new unrelated tabs next to current

--- qutebrowser/.config/qutebrowser/conf/tabs.py
+++ qutebrowser/.config/qutebrowser/conf/tabs.py
@@ -2,5 +2,6 @@ c.tabs.favicons.show = "pinned"
 c.tabs.indicator.width = 2
 c.tabs.last_close = "startpage"
 c.tabs.max_width = 200
+c.tabs.new_position.unrelated = "next"
 c.tabs.position = "top"
 c.tabs.show = "always"
