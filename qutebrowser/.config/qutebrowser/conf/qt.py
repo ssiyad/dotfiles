2f193efb5a345f27618ee77d0ae2680cc068f9ae
Sabu Siyad <hello@ssiyad.com>
2026-07-03 01:11:01 +0530

    qutebrowser: qt args for performance mode and vaapi

--- qutebrowser/.config/qutebrowser/conf/qt.py
+++ qutebrowser/.config/qutebrowser/conf/qt.py
@@ -1,7 +1,9 @@
 c.qt.args = [
+    "performance",
     "enable-gpu-rasterization",
     "ignore-gpu-blacklist",
     "enable-unsafe-webgpu",
+    "enable-features=VaapiVideoDecoder",
 ]
 c.qt.chromium.process_model = "process-per-site"
 c.qt.highdpi = True
