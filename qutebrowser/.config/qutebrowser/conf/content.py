3153b2fddf23bf3664043027d026006b22e40c2f
Sabu Siyad <hello@ssiyad.com>
2026-07-15 20:33:58 +0530

    qutebrowser: allow clipboard access

--- qutebrowser/.config/qutebrowser/conf/content.py
+++ qutebrowser/.config/qutebrowser/conf/content.py
@@ -1,5 +1,3 @@
-c.content.pdfjs = False
-c.content.prefers_reduced_motion = True
 c.content.autoplay = False
 c.content.blocking.adblock.lists = [
     "https://easylist.to/easylist/easylist.txt",
@@ -11,4 +9,10 @@ c.content.blocking.hosts.lists = [
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-only/hosts",
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-only/hosts",
     "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts",
+]
+c.content.javascript.clipboard = "access-paste"
+c.content.pdfjs = False
+c.content.prefers_reduced_motion = True
+c.content.user_stylesheets = [
+    "~/.config/qutebrowser/override_style.css",
 ]
