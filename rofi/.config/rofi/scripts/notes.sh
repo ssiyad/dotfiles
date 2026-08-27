3ec32d8a5418a43a1b91e011f9fad3600a940fda
Sabu Siyad <hello@ssiyad.com>
2026-08-27 15:43:49 +0530

    rofi: entries for notes

---
+++ rofi/.config/rofi/scripts/notes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+case "$1" in
+  journal)
+    nohup alacritty -e nvim -c ":Obsidian today" > /dev/null 2>&1 &
+    exit 0
+    ;;
+  reading_list)
+    nohup alacritty -e nvim ~/notes/reading-list.md > /dev/null 2>&1 &
+    exit 0
+    ;;
+  tasks)
+    nohup alacritty -e nvim ~/notes/tasks.md > /dev/null 2>&1 &
+    exit 0
+    ;;
+esac
+
+echo -e "journal\x00display\x1fJournal"
+echo -e "reading_list\x00display\x1fReading List"
+echo -e "tasks\x00display\x1fTasks"
