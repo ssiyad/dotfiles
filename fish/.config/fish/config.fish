04bf7d43802b79e4c85df16540a538eff0d02d47
Sabu Siyad <hello@ssiyad.com>
2026-09-10 10:53:05 +0530

    feat(fish): auto start niri-session on login on tty1

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,9 @@
+# Bootstrap fisher + install plugins from fish_plugins on a fresh machine.
+if status is-interactive; and not functions -q fisher
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+    fisher update
+end
+
 alias mkdir="mkdir -pv"
 alias ls="ls -aFh --color=auto"
 alias rm="rm -rfv"
@@ -16,14 +22,16 @@ set -g fish_key_bindings fish_vi_key_bindings
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 
-# Bootstrap fisher + install plugins from fish_plugins on a fresh machine
-if status is-interactive; and not functions -q fisher
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-    fisher update
-end
-
 # Set history location to avoid mixing host and container history.
 if test -f /run/.containerenv
     set -l cname (cat /run/.containerenv | grep '^name=' | cut -d'"' -f2 | sed 's/-/_/')
     set -x fish_history "$cname"
+end
+
+# Auto start niri-session on tty1.
+if status is-login; and status is-interactive; and test (tty) = "/dev/tty1"
+    # Check if niri-session is already running for the current user, and start it if not.
+    if not pgrep -xu (whoami) niri-session >/dev/null
+        exec niri-session
+    end
 end
